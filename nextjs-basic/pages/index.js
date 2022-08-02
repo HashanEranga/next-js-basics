5479ecbc7a0e0507631535cd353698b1f91c78c1
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-02 19:00:20 +0530

    navigating to static pages using Link next_Link

--- nextjs-basic/pages/index.js
+++ nextjs-basic/pages/index.js
@@ -1,13 +1,22 @@
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
+import Link from "next/Link";
 
-export default function HomePage(){
-  const router = useRouter()
-  console.log(router.pathname)
-  console.log(router.query)
+export default function HomePage() {
+  const router = useRouter();
+  console.log(router.pathname);
+  console.log(router.query);
 
   return (
     <div>
       <h1>The HomePage</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
