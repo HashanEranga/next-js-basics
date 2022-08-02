eb3e310aec3dec0b2f15b280d21dfe0d0cb7d4a4
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-02 18:49:23 +0530

    use userRouter for view the router path

--- nextjs-basic/pages/clients/[clientProjectId]/index.js
+++ nextjs-basic/pages/clients/[clientProjectId]/index.js
@@ -1,6 +1,10 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 function ClientProjectPage() {
+    const router = useRouter()
+    console.log(router.pathname)
+    console.log(router.query)
   return (
     <div>ClientProjectPage</div>
   )
