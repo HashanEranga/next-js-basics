a8e99a2613aa922f1cb0266f63a74e21c2113572
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-04 14:49:04 +0530

    create btn component and styling

--- next-events-app/components/events/event-item.js
+++ next-events-app/components/events/event-item.js
@@ -1,8 +1,31 @@
+import classes from './event-item.module.css'
+import Button from '../UI/button'
+
 export default function EventItem(props){
-    const {event} = props
+    const {id, title, date, image, location} = props
+    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+        day : 'numeric',
+        month : 'long',
+        year: 'numeric'
+    })
+
+    const formattedAddress = location.replace(', ', '\n')
+    const exploreLink = `/events/${id}`
     return (
-        <li>
-            {event.title}
+        <li key={id} className={classes.item}>
+            <img src={'/'+image} alt={title}/>
+            <div className={classes.summary}>
+                <h2>{title}</h2>
+                <div className={classes.date}>
+                    <p>{humanReadableDate}</p>
+                </div>
+                <div className={classes.address}>
+                    <address>{formattedAddress}</address>
+                </div>
+                <div className={classes.actions}>
+                    <Button link={exploreLink}>Explore Event</Button> 
+                </div>
+            </div>
         </li>
     )
 }
