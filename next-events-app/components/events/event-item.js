6f4e580bebfe2bfeded6bd899c01e6110b83954b
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-04 15:24:10 +0530

    add icons and add button icons

--- next-events-app/components/events/event-item.js
+++ next-events-app/components/events/event-item.js
@@ -1,31 +1,41 @@
-import classes from './event-item.module.css'
-import Button from '../UI/button'
+import classes from "./event-item.module.css";
+import Button from "../UI/button";
+import CalendarIcon from "../UI/Icons/calendar-icon";
+import LocationIcon from "../UI/Icons/location-icon";
+import ArrowIcon from "../UI/Icons/arrow-icon";
 
-export default function EventItem(props){
-    const {id, title, date, image, location} = props
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-        day : 'numeric',
-        month : 'long',
-        year: 'numeric'
-    })
+export default function EventItem(props) {
+  const { id, title, date, image, location } = props;
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-    const formattedAddress = location.replace(', ', '\n')
-    const exploreLink = `/events/${id}`
-    return (
-        <li key={id} className={classes.item}>
-            <img src={'/'+image} alt={title}/>
-            <div className={classes.summary}>
-                <h2>{title}</h2>
-                <div className={classes.date}>
-                    <p>{humanReadableDate}</p>
-                </div>
-                <div className={classes.address}>
-                    <address>{formattedAddress}</address>
-                </div>
-                <div className={classes.actions}>
-                    <Button link={exploreLink}>Explore Event</Button> 
-                </div>
-            </div>
-        </li>
-    )
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+  return (
+    <li key={id} className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.summary}>
+        <h2>{title}</h2>
+        <div className={classes.date}>
+          <CalendarIcon />
+          <p>{humanReadableDate}</p>
+        </div>
+        <div className={classes.address}>
+          <LocationIcon />
+          <address>{formattedAddress}</address>
+        </div>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
 }
