3a849e4863a91feeefe90438ba8aafd3e12e0907
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-05 09:44:53 +0530

    finishing touches are added to the project

--- next-events-app/pages/events/[...slug].js
+++ next-events-app/pages/events/[...slug].js
@@ -1,9 +1,62 @@
-function FilteredEventsPage(){
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/result-title";
+import Button from "../../components/UI/button";
+import ErrorAlert from "../../components/UI/error-alert";
+import { getFilteredEvents } from "../../dummy-data";
+
+function FilteredEventsPage() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+  const filteredYear = +filterData[0];
+  const filteredMonth = +filterData[1];
+
+  const date = new Date(filteredYear, filteredMonth - 1);
+
+  if (
+    isNaN(filteredMonth) ||
+    isNaN(filteredYear) ||
+    filteredYear > 2030 ||
+    filteredYear < 2018 ||
+    filteredMonth > 12 ||
+    filteredMonth < 1
+  ) {
+    return;
+    <>
+      <ErrorAlert>
+        <p>Invalid Filter!</p>;
+      </ErrorAlert>
+      <div className="center">
+        <Button link="/events">Show All Events</Button>
+      </div>
+    </>;
+  }
+
+  if (!filterData) return <p className="center">Loading ...</p>;
+
+  const filteredEvent = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+
+  if (!filteredEvent || filteredEvent.length === 0) {
     return (
-        <div>
-            <h1>Filtered Events Page</h1>
+      <>
+        <ErrorAlert>
+          <p>No events were found for the given filter</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
         </div>
-    )
+      </>
+    );
+  }
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvent} />
+    </>
+  );
 }
 
-export default FilteredEventsPage
+export default FilteredEventsPage;
