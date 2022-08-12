436958c241bc93b795caf85e1475c0e9be80ca3c
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-12 12:47:43 +0530

    prerender the page

--- nextjs-get-static-props/pages/index.js
+++ nextjs-get-static-props/pages/index.js
@@ -1,9 +1,15 @@
+var fs = require('fs/promises')
+var path = require('path')
+
 export default function Home(props) {
   const { products } = props;
   return (
     <div>
       {products.map((product) => {
-        return <li key={product.id}>{product.prodName}</li>;
+        return (
+        <li key={product.id}>{product.prod_name}</li>
+        )
+
       })}
     </div>
   );
@@ -12,13 +18,13 @@ export default function Home(props) {
 // not expose to the cliet side so the database keys and the other keys can be contained in the getStaticProps() method
 // will be redered first prerendered the each page
 export async function getStaticProps() {
+  var filePath = path.join(process.cwd(), 'data', 'DUMMY_DATA.json')
+  var jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(jsonData)
+
   return {
     props: {
-      products: [
-        { id: "1", prodName: "Product 1" },
-        { id: "2", prodName: "Product 2" },
-        { id: "3", prodName: "Product 3" },
-      ],
+      products: data.products
     },
   };
 }
