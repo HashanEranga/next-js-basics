b1c30a5056b38b407239141bffaa5e2a4046c7b3
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-14 02:38:44 +0530

    user data fetching using useEffect and useSWR

---
+++ nextjs-get-static-props/pages/sales-products/index.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+export default function SalesProductsPage() {
+  //   const [isLoading, setIsLoading] = useState(false);
+  const [sales, setSale] = useState();
+  const { data, error } = useSWR(
+    "https://sales-proj-default-rtdb.firebaseio.com/sales.json", Fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      const salesList = [];
+      for (const key in data) {
+        //   console.log(key);
+        salesList.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      // setIsLoading(false);
+      setSale(salesList);
+    }
+  }, [data]);
+
+  if (error) {
+    return <h1>Error Occured</h1>;
+  }
+
+  if (!data || !sales) {
+    return <h1>Loading ... </h1>;
+  }
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch("https://sales-proj-default-rtdb.firebaseio.com/sales.json")
+  //       .then((responce) => responce.json())
+  //       .then((data) => {
+  //         const salesList = [];
+  //         for (const key in data) {
+  //           console.log(key);
+  //           salesList.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setIsLoading(false);
+  //         setSale(salesList);
+  //         console.log(salesList);
+  //       });
+  //   }, []);
+
+  //   if (isLoading) {
+  //     return <h1>Loading ...</h1>;
+  //   }
+
+  //   if (!sales) {
+  //     return <h1>No data to show</h1>;
+  //   }
+
+  return (
+    <>
+      <h1>Sales List</h1>
+      <ul>
+        {/* {console.log(sales)} */}
+        {sales.map((saleItem) => (
+          <li key={saleItem.id}>
+            {saleItem.username} - ${saleItem.volume}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function Fetcher(url) {
+    return fetch(url, {
+     headers: {
+      'Content-Type': 'application/json',
+     },
+    }).then(response => response.json());
+   }
+   
