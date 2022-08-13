8e4653cd2581c93a1a72111192b7c6c7124bd3f9
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-13 21:16:19 +0530

    remove paths and fallback pages with not found

--- nextjs-get-static-props/pages/[pid]/index.js
+++ nextjs-get-static-props/pages/[pid]/index.js
@@ -18,15 +18,25 @@ export default function ProjectDetailPage(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  var { params } = context;
-  var productId = params.pid;
-
+async function getData(){
   var filePath = path.join(process.cwd(), "data", "DUMMY_DATA.json");
   var jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  var { params } = context;
+  var productId = params.pid;
+
+  const data = await getData()
+ 
   var product = data.products.find((product) => product.id === productId);
+
+  if (!product){
+    return {notFound: true}
+  }
 
   return {
     props: {
@@ -37,15 +47,11 @@ export async function getStaticProps(context) {
 
 // since prerendering need to notify what are the paths to pre generate them
 export async function getStaticPaths() {
+  const data = await getData()
+  const ids = data.products.map(product => product.id)
+  const pathsWithParams = ids.map((id)=>({params : {pid : id}}))
     return {
-        paths : [
-            {params : {pid : "p1"}},
-            {params : {pid : "p2"}},
-            {params : {pid : "p3"}},
-            // {params : {pid : "p4"}},
-            // {params : {pid : "p5"}},
-            // {params : {pid : "p6"}},
-        ],
+        paths : pathsWithParams,
         // all the dynamic paths must be defined
         // fallback : false 
         // will gen the page dynamically need to handle 
