60aea12e8b2a4d38746ada8cad1d09bcc25cb533
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-16 19:51:48 +0530

    make static generate the starting home screen

--- next-events-app/pages/index.js
+++ next-events-app/pages/index.js
@@ -1,10 +1,21 @@
 import EventList from '../components/events/event-list'
-import { getFeaturedEvents } from '../dummy-data'
-export default function Home() {
-  const featuredEventsHandler = getFeaturedEvents()
+import { getFeaturedEvents } from '../helper/api-util'
+
+export default function Home(props) {
   return (
     <div>
-      <EventList items = {featuredEventsHandler}/>
+      <EventList items = {props.events}/>
     </div>
   )
+}
+
+
+export async function getStaticProps(){
+    const featuredEvents = await getFeaturedEvents()
+
+    return {
+        props: {
+            events : featuredEvents
+        }
+    }
 }
